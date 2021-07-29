d7552cd2590b890bab598a355221347512b84dcb
helloyxw <helloyxw@163.com>
2021-07-29 16:43:18 +0800

    update

--- webhook.js
+++ webhook.js
@@ -1,7 +1,25 @@
+const crypto = require("crypto");
 let http = require("http");
+const SECRET = '123456'
+function sign(body) {
+    return `sha1=` + crypto.createHmac('sha1', SECRET).update(body).digest('hex')
+}
 let server = http.createServer(function(req,res) {
     console.log(req.method, req.url);
     if(req.method == 'POST' && req.url == '/webhook') {
+        let buffers = [];
+        req.on('data', function(buffer) {
+            buffers.push(buffer)
+        })
+        req.on('end', function(buffer) {
+            let body = Buffer.concat(buffers)
+            let event = req.header['x-github-event']; // event=push
+            // github请求来的时候，要传递请求体body，另外还会传一个signature过来，你需要验证签名对不对
+            let signature = req.headers['x-hub-signature']
+            if(signature !== sign(body)) {
+                return res.end('Not Allowed')
+            }
+        })
         res.setHeader('Content-Type', 'application/json')
         res.end(JSON.stringify({ok: true}))
     } else {
